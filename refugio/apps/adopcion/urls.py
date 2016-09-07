bcf51b51b5745175976a290e16f28a92a47e89f9
root <shellfity@gmail.com>
2016-09-07 15:50:14 -0500

    proyectos que no van

--- refugio/apps/adopcion/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url, include
-from apps.adopcion.views import index
-
-urlpatterns = [
-    url(r'^$', index),
-]
--- refugio/apps/mascota/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from apps.mascota.models import Vacuna, Mascota
-
-# Register your models here.
-admin.site.register(Mascota)
-admin.site.register(Vacuna)
